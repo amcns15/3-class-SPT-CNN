fixed normalisation

run_model/run_model.py
@@ -16,7 +16,7 @@ def preprocess_chunk(chunk):
     # print("chunk percentiles pre processing", np.percentile(chunk, [10, 25, 50, 75, 90]))
 
    # chunk = chunk.astype(np.float32)
-    chunk = chunk / 255.0
+    chunk = chunk / 65535.0
     chunk = chunk[..., None] #Channel number axis
     chunk = chunk[None, ...] #Batch size axis
 
@@ -75,10 +75,10 @@ def predict_states(video, model, store_frame_labels ,name, chunk_size = 5, class
     for frame in np.arange(start, end, 1):
         store_frame_labels.iloc[frame, 0] = label
 
-    # # OPTIONAL SCRIPT TO HOMOGENISE RESULTS
-    for chunk in range(1, len(results) - 1):
-        if results.iloc[chunk - 1]["predicted_label"] == results.iloc[chunk + 1]["predicted_label"]:
-            results.loc[chunk, "predicted_label"] = results.iloc[chunk - 1]["predicted_label"]        
+#    # OPTIONAL SCRIPT TO HOMOGENISE RESULTS
+#     for chunk in range(1, len(results) - 1):
+#         if results.iloc[chunk - 1]["predicted_label"] == results.iloc[chunk + 1]["predicted_label"]:
+#             results.loc[chunk, "predicted_label"] = results.iloc[chunk - 1]["predicted_label"]        
 
     return results, store_frame_labels
 
@@ -86,12 +86,13 @@ def predict_states(video, model, store_frame_labels ,name, chunk_size = 5, class
 
 
 if __name__ == "__main__":
+    print("here")
 
     input_dir = Path(r"\\rivendell.physics.ox.ac.uk\user\students\2024\jesu4837\summer_internship\real_data\scaled_and_full")
 
     #input_video = tiff.imread(r"\\rivendell.physics.ox.ac.uk\user\students\2024\jesu4837\summer_internship\real_data\scaled\69_scaled.tif")
 
-    model_loaded = keras.saving.load_model(r"C:\Users\jesu4837\Downloads\model_local_store\aug_17_fine_tuned.keras",
+    model_loaded = keras.saving.load_model(r"C:\Users\jesu4837\Downloads\model_local_store\sept_1_fine_tuned.keras",
                                             compile = False,
                                            custom_objects = {"Conv2Plus1D": Conv2Plus1D} )
 
@@ -111,13 +112,13 @@ if __name__ == "__main__":
 
         name = vid_stem.split("_")[0]
         
-        store_frame_labels_df.to_csv(rf"H:\summer_internship\analysis\label_per_frame\{name}_per_frame_label.csv")
+        store_frame_labels_df.to_csv(rf"H:\summer_internship\analysis\label_per_frame\{name}_per_frame_label_FT.csv")
 
        
         tinted_video = build_tinted_video(input_video, results_df)
-        tiff.imwrite(rf"\\rivendell.physics.ox.ac.uk\user\students\2024\jesu4837\summer_internship\run_model\labelled_videos\{vid_stem}_labelled_video.tif", tinted_video, photometric="rgb")
+        tiff.imwrite(rf"\\rivendell.physics.ox.ac.uk\user\students\2024\jesu4837\summer_internship\run_model\labelled_videos\{vid_stem}_labelled_video_FT.tif", tinted_video, photometric="rgb")
 
-       # plot_states(results_df)
+    #    # plot_states(results_df)
         colour_khymograph(vid_stem.split("_")[0], results_df)
 
         print(results_df)